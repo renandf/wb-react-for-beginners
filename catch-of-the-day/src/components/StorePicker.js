f1c76d0654ba77ee600330e48c7c6adb20a106fa
Renan <renandf@gmail.com>
2018-11-28 16:59:41 +1100

    Initial commit - classes 1 to 10

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+	render() {
+		return (
+			<form className="store-selector">
+				<h2>Please Enter a Store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<button type="submit">Visit store →</button>
+			</form>
+		);
+	}
+}
+
+export default StorePicker;
