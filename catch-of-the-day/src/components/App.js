478aaca0aaedc052502d2492741cd7bec87a1cc0
Renan <renandf@gmail.com>
2018-12-01 18:13:56 +1100

    completed classes 11 to 16; add fish to inventory and control state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,56 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+
+    // add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    
+    // set the new fishes object to state
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes} )
+  };
+
+  addToOrder = key => {
+    // take a copy of state
+    const order = { ...this.state.order };
+    // either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // call setState to update our state object
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,24 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		// Stop the form from submitting
+		event.preventDefault();
+		
+		// Get the text from that input
+		const storeName = this.myInput.value.value;
+
+		// Change the page to /store/whatever-they-entered
+		this.props.history.push(`/store/${storeName}`);
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter a Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
 				<button type="submit">Visit store →</button>
 			</form>
 		);
